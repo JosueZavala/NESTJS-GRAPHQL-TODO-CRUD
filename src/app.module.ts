77d013f6edd9d245491ca13587a7d7df1dc102e8
Josue Zavala <josue.zavala123@gmail.com>
2024-12-11 14:00:50 -0600

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { HelloWorldModule } from './hello-world/hello-world.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    HelloWorldModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
