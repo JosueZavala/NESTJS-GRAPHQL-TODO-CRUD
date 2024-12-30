ce0e9707f30ef733aa1e689bb7c659ed045ae29b
Josue Zavala <josue.zavala123@gmail.com>
2024-12-29 21:09:26 -0600

    Adding filters to FindAll query

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Todo } from './entity/todo.entity';
 import { CreateTodoInput, UpdateTodoInput } from './dto/inputs';
+import { StatusArgs } from './dto/args/status.args';
 
 @Injectable()
 export class TodoService {
@@ -10,7 +11,11 @@ export class TodoService {
     { id: 3, description: 'piedra del Poder', done: false },
   ];
 
-  findAll(): Todo[] {
+  findAll(statusArgs: StatusArgs): Todo[] {
+    const { status } = statusArgs;
+    if (status !== undefined)
+      return this.todos.filter((todo) => todo.done === status);
+
     return this.todos;
   }
 
