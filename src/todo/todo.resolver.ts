e7d0fe82a7713fdf567702cbd751c065355bee61
Josue Zavala <josue.zavala123@gmail.com>
2024-12-29 20:41:46 -0600

    Adding dtos and logic for get 1 or more todos, create it, or update it

---
+++ src/todo/todo.resolver.ts
@@ -0,0 +1,31 @@
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Todo } from './entity/todo.entity';
+import { TodoService } from './todo.service';
+import { CreateTodoInput, UpdateTodoInput } from './dto/inputs';
+
+@Resolver()
+export class TodoResolver {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Query(() => [Todo], { name: 'todos' })
+  findAll(): Todo[] {
+    return this.todoService.findAll();
+  }
+
+  @Query(() => Todo, { name: 'todo' })
+  findOne(@Args('id', { type: () => Int }) id: number) {
+    return this.todoService.findOne(id);
+  }
+
+  @Mutation(() => Todo, { name: 'createTodo' })
+  createTodo(@Args('createTodoInput') createTodoInput: CreateTodoInput) {
+    return this.todoService.create(createTodoInput);
+  }
+
+  @Mutation(() => Todo, { name: 'updateTodo' })
+  updateTodo(@Args('updateTodoInput') updateTodoInput: UpdateTodoInput) {
+    return this.todoService.update(updateTodoInput);
+  }
+
+  removeTodo() {}
+}
